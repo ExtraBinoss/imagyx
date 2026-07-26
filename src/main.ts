fac2e7b9b33b5f7e1530dd8ac8a6d16d910b863e
ExtraBinoss <113912449+ExtraBinoss@users.noreply.github.com>
2026-07-26 16:41:54 +0200

    Load virtual grid styles

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import './style.css'
+import './virtual-grid.css'
 
 const savedTheme = localStorage.getItem('imagyx-theme')
 const resolvedTheme =
