259630fd56384f47387d97226c3c626dd099e554
ExtraBinoss <113912449+ExtraBinoss@users.noreply.github.com>
2026-07-27 00:24:44 +0200

    refactor(ui): style button groups through slots

--- src/components/ui/ButtonGroup/ButtonGroup.vue
+++ src/components/ui/ButtonGroup/ButtonGroup.vue
@@ -21,16 +21,14 @@ withDefaults(defineProps<{ full?: boolean }>(), { full: false })
   box-shadow: inset 0 1px 3px rgb(15 23 42 / 0.07), inset 0 1px 0 rgb(255 255 255 / 0.24);
 }
 .ui-button-group--full { width: 100%; }
-.ui-button-group :deep(.ui-button) {
+.ui-button-group :slotted(.ui-button) {
   flex: 1 1 0;
   min-width: 0;
   border-color: transparent;
   box-shadow: none;
 }
-.ui-button-group :deep(.ui-button:hover:not(:disabled)) {
-  transform: none;
-}
-.ui-button-group :deep(.ui-button--pressed) {
+.ui-button-group :slotted(.ui-button:hover:not(:disabled)) { transform: none; }
+.ui-button-group :slotted(.ui-button--pressed) {
   border-color: var(--border);
   background: var(--surface-elevated);
   color: var(--text);
@@ -40,7 +38,7 @@ withDefaults(defineProps<{ full?: boolean }>(), { full: false })
     0 1px 2px rgb(15 23 42 / 0.08),
     0 5px 12px -10px rgb(15 23 42 / 0.35);
 }
-:root[data-theme='dark'] .ui-button-group {
+:global(:root[data-theme='dark']) .ui-button-group {
   background: color-mix(in srgb, var(--background) 76%, black);
   box-shadow: inset 0 1px 4px rgb(0 0 0 / 0.32), inset 0 1px 0 rgb(255 255 255 / 0.04);
 }
